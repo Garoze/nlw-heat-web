95ed0e092e4035b2b99be5fb6784fb88eae8866a
Garoze <marcelogaroze@hotmail.com>
2021-10-21 15:34:30 -0300

    feat: Get last 3 messages from the backend and list them on the MessageList component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,50 +1,47 @@
-import logoImg from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
 
+import { api } from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
 import styles from './styles.module.scss';
 
+type User = {
+  name: string;
+  avatar_url: string;
+};
+
+type Message = {
+  id: string;
+  message: string;
+  user: User;
+};
+
 export const MessageList = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last').then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/garoze.png" alt="Garoze" />
-            </div>
-            <span>Garoze (.Dev)</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/garoze.png" alt="Dev" />
-            </div>
-            <span>O tal do .Dev</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/garoze.png" alt="Dev" />
-            </div>
-            <span>O tal do .Dev</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li className={styles.message} key={message.id}>
+              <p className={styles.messageContent}>{message.message}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
